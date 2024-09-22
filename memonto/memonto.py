7732f0f36572a9e9fd6cf675f520bf1a2928d2d6
shihanwan <shihanwan@gmail.com>
2024-09-22 17:46:30 -0400

    add configure tests

--- memonto/memonto.py
+++ memonto/memonto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict, Field 
+from pydantic import BaseModel, ConfigDict, Field
 from rdflib import Graph, Namespace, URIRef
 from typing import Optional, Union
 
