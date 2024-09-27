f9513cf64efda53e37efeb991b064aef0234573b
shihanwan <shihanwan@gmail.com>
2024-09-27 13:33:24 -0400

    handle valueerror on vector delete

--- memonto/core/forget.py
+++ memonto/core/forget.py
@@ -1,5 +1,6 @@
 from memonto.stores.triple.base_store import TripleStoreModel
 from memonto.stores.vector.base_store import VectorStoreModel
+from memonto.utils.logger import logger
 
 
 def forget_memory(
@@ -7,8 +8,13 @@ def forget_memory(
     triple_store: TripleStoreModel,
     vector_store: VectorStoreModel,
 ) -> None:
-    if vector_store:
-        vector_store.delete(id)
+    try:
+        if vector_store:
+            vector_store.delete(id)
 
-    if triple_store:
-        triple_store.delete(id)
+        if triple_store:
+            triple_store.delete(id)
+    except ValueError as e:
+        logger.warning(e)
+    except Exception as e:
+        logger.error(e)
